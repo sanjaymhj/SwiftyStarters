d76ace3c77c599baa2e40f2fcd9de5a57e463cbc
Sanjay Maharjan <sanjaymaharjan@lftechnology.com>
2017-06-16 16:22:54 +0545

    uicolorextension convert hex color to uicolor

---
+++ Extensions/UIColor+Extension.swift
@@ -0,0 +1,16 @@
+import Foundation
+import UIKit
+
+public extension UIColor {
+  /**
+   Initializer from Hex Code. Hex code doesnot include alpha value.
+   */
+  convenience init(hex: Int, alpha: CGFloat = 1.0) {
+    let red     = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+    let green   = CGFloat((hex & 0xFF00) >> 8) / 255.0
+    let blue    = CGFloat((hex & 0xFF)) / 255.0
+    
+    self.init(red:red, green:green, blue:blue, alpha:alpha)
+  }
+
+}
