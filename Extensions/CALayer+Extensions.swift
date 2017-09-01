373386f0cdd30120a9c3dd3fe49cc7f4b7b138ce
Kusal <shrstha.kusal@gmail.com>
2017-09-01 11:15:08 +0545

    shrksl: Added CALayer Extension

---
+++ Extensions/CALayer+Extensions.swift
@@ -0,0 +1,15 @@
+import Foundation
+import QuartzCore
+
+extension CALayer {
+  
+  public class func animateWithDuration(duration: TimeInterval, animation: () -> Void, completion: (() -> Void)?) {
+    CATransaction.begin()
+    CATransaction.setAnimationDuration(duration)
+    CATransaction.setCompletionBlock(completion)
+    animation()
+    CATransaction.commit()
+  }
+
+}
+
